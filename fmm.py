e99f37e72a2181254360be4eae702245ea4f4dc4
roflmaostc <f.wechsler@tum.de>
2020-07-08 10:23:36 +0200

    Clean code

--- fmm.py
+++ fmm.py
@@ -8,7 +8,7 @@ from scipy.fftpack import fft
 from scipy.sparse import diags
 
 
-def fmm1d_te_layer_modes(perm, period, k0, kx, N):
+def fmm1d_te_layer_modes(perm, period, k_0, k_x, N):
     '''Calculates the TE eigenmodes of a one-dimensional grating layer.
 
     Arguments
@@ -17,9 +17,9 @@ def fmm1d_te_layer_modes(perm, period, k0, kx, N):
             permittivity distribution
         period: float
             grating period
-        k0: float
+        k_0: float
             vacuum wavenumber
-        kx: float
+        k_x: float
             transverse wave vector
         N: int
             number of positive Fourier orders
@@ -32,9 +32,10 @@ def fmm1d_te_layer_modes(perm, period, k0, kx, N):
             Fourier coefficients of the eigenmodes (each column
             corresponds to one mode)
     '''
-    Nx = perm.size
+    # number of points in x direction
+    N_x = perm.size
     # Fourier coefficients of the permittivity
-    perm_fc = fft(perm) / (Nx - 1)
+    perm_fc = fft(perm) / (N_x - 1)
 
     # take the first 2 * N positive and 0 frequency
     perm_fc_pos = perm_fc[:2 * N + 1]
@@ -45,13 +46,13 @@ def fmm1d_te_layer_modes(perm, period, k0, kx, N):
     # calculate grating
     Gm = np.arange(-N, N + 1, 1) * 2 * np.pi / period
 
-    # create the toeplitz matrix containing the Fourier coefficients of perm
+    # create the Toeplitz matrix containing the Fourier coefficients of perm
     eps_hat = toeplitz(perm_fc_pos, perm_fc_neg)
     # create \hat K Matrix
-    K_hat_square = diags((Gm + kx) ** 2, offsets=0).todense()
+    K_hat_square = diags((Gm + k_x) ** 2, offsets=0).todense()
 
     # create final matrix
-    M_hat = (k0 ** 2 * eps_hat - K_hat_square)
+    M_hat = (k_0 ** 2 * eps_hat - K_hat_square)
 
     # calculate the eigenvalues and eigenvectors of M_hat
     eig_values, eig_vectors = eig(M_hat)
@@ -59,14 +60,13 @@ def fmm1d_te_layer_modes(perm, period, k0, kx, N):
     # take sqrt to get the propagation constant
     beta = np.sqrt(eig_values, dtype=np.complex128)
     # invert eigenvalue if it corresponds to a backward propagating direction
-    beta[np.where(np.real(beta) + np.imag(beta) < 0)] *= -1
+    beta[np.real(beta) + np.imag(beta) < 0] *= -1
 
     return beta, eig_vectors
 
 
-
 def fmm1d_te(lam, theta, period, perm_in, perm_out,
-             layer_perm, layer_thicknesses, N):
+             layer_perm, layer_thicknesses, N, dtype=np.complex128):
     '''Calculates the TE diffraction efficiencies for a one-dimensional
     layered grating structure using the T-matrix method.
 
@@ -104,26 +104,26 @@ def fmm1d_te(lam, theta, period, perm_in, perm_out,
             diffraction orders
     '''
     # vacuum wave vector
-    k0 = 2 * np.pi / lam + 0j
+    k_0 = 2 * np.pi / lam + 0j
     # x component of k
-    kx = k0 * np.sqrt(perm_in) * np.sin(theta)
-    G = (2 * np.pi / period) * np.arange(-N, N+1)
+    k_x = k_0 * np.sqrt(perm_in) * np.sin(theta)
+    # create grating
+    G = (2 * np.pi / period) * np.arange(-N, N + 1, dtype=dtype)
     # create K_hat matrix
-    K_hat_square = np.diag((kx + G) ** 2)
+    K_hat_square = np.diag((k_x + G) ** 2)
     # initial phi electrical
-    ident = np.identity(2 * N + 1,dtype=np.complex128)
+    ident = np.identity(2 * N + 1, dtype=dtype)
 
     # initial beta
-    beta_0_hat = np.sqrt(k0**2 * perm_in * ident - K_hat_square, dtype=np.complex128)
-    #beta = np.diagonal(beta_0_hat)
-    beta_0_hat[np.where(np.real(beta_0_hat) + np.imag(beta_0_hat) < 0.0)] *= -1
+    beta_0_hat = np.sqrt(k_0 ** 2 * perm_in * ident - K_hat_square, dtype=dtype)
+    beta_0_hat[np.real(beta_0_hat) + np.imag(beta_0_hat) < 0.0] *= -1
     # initial block matrix
     T_matrix = np.block([[ident, ident],
-                        [beta_0_hat, - beta_0_hat]])
+                        [beta_0_hat, -beta_0_hat]])
     # iterate over all z layers
     for lt, perm in zip(layer_thicknesses, layer_perm):
-
-        beta, phi_e = fmm1d_te_layer_modes(perm, period, k0, kx, N)
+        # get the betas and phi_e in this layer
+        beta, phi_e = fmm1d_te_layer_modes(perm, period, k_0, k_x, N)
         # convert beta to beta_hat containing the entries on the diagonal
         beta_hat = np.diag(beta)
 
@@ -131,49 +131,54 @@ def fmm1d_te(lam, theta, period, perm_in, perm_out,
         p_pos = np.diag(np.exp(1j * beta * lt))
         p_neg = np.diag(np.exp(-1j * beta * lt))
 
+        # create A matrix which is needed to get the new transfer matrix
         A = np.block([[phi_e, phi_e],
                       [np.dot(phi_e, beta_hat),
                        np.dot(-phi_e, beta_hat)]])
-
+        # put the propagation matrices in a block matrix
         p_mat = np.block([[p_pos, np.zeros(p_pos.shape)],
-                                  [np.zeros(p_pos.shape), p_neg]])
-        T = A @ np.linalg.solve(A.T, p_mat.T).T
+                          [np.zeros(p_pos.shape), p_neg]])
+        T = A @ solve(A.T, p_mat.T).T
         T_matrix = T @ T_matrix
 
     # beta_out_hat matrix
-    beta_out_hat = np.sqrt(k0 ** 2 * perm_out * ident
+    beta_out_hat = np.sqrt(k_0 ** 2 * perm_out * ident
                            - K_hat_square, dtype=np.complex128)
-    beta_out_hat[np.where(np.real(beta_out_hat) + np.imag(beta_out_hat) < 0.0)] *= -1
-    #beta = np.diagonal(beta_out_hat)
+    beta_out_hat[np.real(beta_out_hat) + np.imag(beta_out_hat) < 0.0] *= -1
+    # last missing matrix which inverse is left multiplied
     T_final = np.block([[ident, ident],
                         [beta_out_hat, - beta_out_hat]])
-    T_matrix = np.linalg.solve(T_final, T_matrix)
+
+    # create the final transfer matrix
+    T_matrix = solve(T_final, T_matrix)
 
     # initial amplitudes
-    a_in = np.zeros(2 * N + 1,dtype=np.complex128)
+    a_in = np.zeros(2 * N + 1, dtype=dtype)
     # set only this input coefficient to 1
     a_in[N] = 1 + 0j
 
     # extract the four block matrices from the T_matrix
     index_1 = slice(None, 2 * N + 1)
     index_2 = slice(2 * N + 1, None)
-    
+
     t11 = T_matrix[index_1, index_1]
     t12 = T_matrix[index_1, index_2]
     t21 = T_matrix[index_2, index_1]
     t22 = T_matrix[index_2, index_2]
 
     # calculate R and T matrices
-    r = np.dot(-np.linalg.solve(t22, t21), a_in[:, np.newaxis])
-    t = np.dot((t11 - t12 @ np.linalg.solve(t22, t21)), a_in[:, np.newaxis])
-    r = r.A1
-    t = t.A1
+    r = np.dot(-solve(t22, t21), a_in[:, np.newaxis])
+    t = np.dot((t11 - t12 @ solve(t22, t21)), a_in[:, np.newaxis])
 
-    # extract efficiencies
-    
+    # extract the diagonal elements
     beta_in = np.diag(beta_0_hat)
-    beta_out = np.diag(beta_out_hat)
-    eta_r = np.real(r*np.conj(r)*beta_in) / np.real(beta_in[N])
-    eta_t = np.real(t*np.conj(t)*beta_in) / np.real(beta_out[N])
-    
+
+    # calculate transmission and reflection efficiencies
+    eta_r = np.array(np.real(1 / np.real(beta_in[N]) *
+                             np.dot(np.real(beta_0_hat),
+                                    np.multiply(r, np.conj(r)))))
+    eta_t = np.array(np.real(1 / np.real(beta_in[N]) *
+                             np.dot(np.real(beta_out_hat),
+                                    np.multiply(t, np.conj(t)))))
+
     return eta_r, eta_t, r, t
