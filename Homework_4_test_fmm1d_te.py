ffdd4c28b24fada0e08b2b264fe06a7240e6fef6
roflmaostc <f.wechsler@tum.de>
2020-07-08 10:46:18 +0200

    Add some timing and results for different N and thicknesses

--- Homework_4_test_fmm1d_te.py
+++ Homework_4_test_fmm1d_te.py
@@ -4,9 +4,10 @@
 import numpy as np
 from fmm import fmm1d_te
 from matplotlib import pyplot as plt
+import time as time 
 
 plt.rcParams.update({
-        'figure.figsize': (12/2.54, 9/2.54),
+       'figure.figsize': (12/2.54, 9/2.54),
         'figure.subplot.bottom': 0.145,
         'figure.subplot.left': 0.165,
         'figure.subplot.right': 0.9,
@@ -18,6 +19,24 @@ plt.rcParams.update({
 })
 
 plt.close('all')
+
+
+def print_results(N, theta, eta_t, eta_r, thicknesses):
+    # filter for non-zero entries
+    eta_r = eta_r[eta_r > 0]
+    eta_t = eta_t[eta_t > 0]
+    # printing
+    print("Number of positive Fourier orders:\t{}".format(N))
+    print("Layer thicknesses:\t\t\t{}".format(thicknesses))
+    print("Angle:\t\t\t\t\t{:.4f}".format(theta*180/np.pi))
+    print("Sum of eta_t and eta_r:\t\t\t{:.4f}".format(np.sum(eta_t) +
+                                                     np.sum(eta_r)))
+    print("Number of reflection orders:\t\t{}".format(len(eta_r)))
+    print("Number of transmission orders:\t\t{}".format(len(eta_t)))
+    print("eta_r:\t\t\t\t\t{}".format(eta_r))
+    print("eta_t:\t\t\t\t\t{}".format(eta_t))
+    print("\n")
+    return 0
 
 # %% parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 lam         = 1.064  # vacuum wavelength [µm]
@@ -43,15 +62,26 @@ for i in range(len(widths)):
     layer_perm[i, x <= widths[i]] = perm_h
 
 
+for N in np.arange(10, 40, 5):
+    eta_r, eta_t, r, t = fmm1d_te(lam, theta, period, perm_in, perm_out,
+                                  layer_perm, thicknesses, N)
+    _ = print_results(N, theta, eta_t, eta_r, thicknesses)
+
+N = 20
+thicknesses = np.array([1, 1, 1]) * 0.5  # thicknesses of the
 eta_r, eta_t, r, t = fmm1d_te(lam, theta, period, perm_in, perm_out,
                               layer_perm, thicknesses, N)
-# filter for non-zero entries
-eta_r = eta_r[eta_r > 0]
-eta_t = eta_t[eta_t > 0]
+_ = print_results(N, theta, eta_t, eta_r, thicknesses)
 
-print("Angle:\t\t\t\t{:.4f}".format(theta*180/np.pi))
-print("Sum of eta_t and eta_r:\t\t{:.4f}".format(np.sum(eta_t) + np.sum(eta_r)))
-print("Number of reflection orders:\t{}".format(len(eta_r)))
-print("Number of transmission orders:\t{}".format(len(eta_t)))
-print("eta_r:\t\t\t\t{}".format(eta_r))
-print("eta_t:\t\t\t\t{}".format(eta_t))
+
+for dtype in [np.complex64, np.complex128]:
+    times = []
+    for _ in range(30):
+        a = time.time()
+        eta_r, eta_t, r, t = fmm1d_te(lam, theta, period, perm_in, perm_out,
+                                  layer_perm, thicknesses, N, dtype=dtype)
+        b = time.time()
+        times.append(b - a)
+
+    print("Time elapsed for {}:\t({:.4f} ± {:.4f})s".format(dtype, np.mean(times),
+                                                        np.std(times)))
